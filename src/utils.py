eb8083eb19c108213d17370b030e6ae82b7d8cbb
Toninhooo <tony.ribeiro1717@gmail.com>
2025-12-20 15:33:27 +0000

    prediction pipeline done with app.py

--- src/utils.py
+++ src/utils.py
@@ -9,10 +9,6 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
 from src.logger import logging
-import warnings
-from sklearn.exceptions import ConvergenceWarning
-
-warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
 def save_object(file_path, obj):
     '''
@@ -64,5 +60,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
         
         return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
